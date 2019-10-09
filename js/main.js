6550d13306b673fbac63083d4a32b844e3e7296f
nilyukov <nilyukov@gmail.com>
2019-10-09 11:42:21 +0300

    More details

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@ var generateRandomOffers = function () {
         guests: 4,
         checkin: CHECKIN[Math.floor(Math.random() * CHECKIN.length)],
         checkout: CHECKOUT[Math.floor(Math.random() * CHECKOUT.length)],
-        features: FEATURES.slice(0, Math.floor(Math.random() * (FEATURES.length - 1)) + 1),
+        features: FEATURES.slice(0, Math.floor(Math.random() * FEATURES.length) + 1),
         description: 'description',
-        photos: PHOTOS.slice(0, Math.floor(Math.random() * (PHOTOS.length - 1)) + 1)
+        photos: PHOTOS.slice(0, Math.floor(Math.random() * PHOTOS.length) + 1)
       },
       location: {
         x: pinX,
@@ -56,6 +56,60 @@ var renderPin = function (descriptionPin) {
   return pinElement;
 };
 
+var renderCard = function (descriptionPin) {
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = descriptionPin.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = descriptionPin.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = descriptionPin.offer.price + '₽/ночь';
+
+  var offerType;
+  switch (descriptionPin.offer.type) {
+    case 'flat':
+      offerType = 'Квартира';
+      break;
+    case 'bungalo':
+      offerType = 'Бунгало';
+      break;
+    case 'house':
+      offerType = 'Дом';
+      break;
+    case 'palace':
+      offerType = 'Дворец';
+      break;
+    default:
+      offerType = 'Жилье';
+      break;
+  }
+  cardElement.querySelector('.popup__type').textContent = offerType;
+  cardElement.querySelector('.popup__text--capacity').textContent = descriptionPin.offer.rooms + ' комнаты для ' + descriptionPin.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + descriptionPin.offer.checkin + ', выезд до ' + descriptionPin.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = descriptionPin.offer.description;
+
+  for (var i = 0; i < descriptionPin.offer.features.length; i++) {
+    var featuresElement = '<li class="popup__feature popup__feature--' + descriptionPin.offer.features[i] + '"></li>';
+    if (i === 0) {
+      cardElement.querySelector('.popup__features').innerHTML = featuresElement;
+    } else {
+      cardElement.querySelector('.popup__features').insertAdjacentHTML('beforeend', featuresElement);
+    }
+  }
+
+  for (var j = 0; j < descriptionPin.offer.photos.length; j++) {
+    var photosElement = '<img src="' + descriptionPin.offer.photos[j] + '" class="popup__photo" width="45" height="40" alt="Фотография жилья">';
+    if (j === 0) {
+      cardElement.querySelector('.popup__photos').innerHTML = photosElement;
+    } else {
+      cardElement.querySelector('.popup__photos').insertAdjacentHTML('beforeend', photosElement);
+    }
+  }
+
+  cardElement.querySelector('.popup__avatar').src = descriptionPin.author.avatar;
+
+  return cardElement;
+};
+
+
 var fillMap = function () {
   var listElement = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
@@ -63,10 +117,10 @@ var fillMap = function () {
 
   for (var i = 0; i < COUNT_DESCRIPTIONS; i++) {
     fragment.appendChild(renderPin(descriptionOffers[i]));
+    fragment.appendChild(renderCard(descriptionOffers[i]));
   }
   listElement.appendChild(fragment);
 };
-
 
 document.querySelector('.map').classList.remove('map--faded');
 fillMap();
